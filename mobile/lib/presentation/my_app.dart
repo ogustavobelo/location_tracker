b6e277bffcf49666d0539b2cc7e91f85fc88b228
GUSTAVO BELO <francisco.belo@unimedfortaleza.com.br>
2021-04-17 14:03:27 -0300

    global toast added

--- mobile/lib/presentation/my_app.dart
+++ mobile/lib/presentation/my_app.dart
@@ -1,14 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:location_tracker/core/themes/themes.dart';
 import 'package:location_tracker/presentation/home/home_screen.dart';
+import 'package:overlay_support/overlay_support.dart';
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Location tracker',
-      theme: Themes.defaultTheme,
-      home: HomeScreen(),
+    return OverlaySupport(
+      child: MaterialApp(
+        title: 'Location tracker',
+        theme: Themes.defaultTheme,
+        home: HomeScreen(),
+      ),
     );
   }
 }
